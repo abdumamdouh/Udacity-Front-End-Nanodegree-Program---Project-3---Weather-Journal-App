d213325356db38c3e3b6f64aa7d505681b2a13a7
abdumamdouh <eabdogfx@gmail.com>
2021-05-13 07:29:52 +0200

    .

--- server.js
+++ server.js
@@ -1,22 +1,47 @@
 // Setup empty JS object to act as endpoint for all routes
+const projectData = {};
+
 // Express to run server and routes
+
+const express = require('express');
 
 // Start up an instance of app
 
-/* Dependencies */
-/* Middleware*/
+const app = express();
 
+/* Dependencies */
+const bodyParser = require('body-parser');
+
+/* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 // Cors for cross origin allowance
+
+const cors = require('cors');
+app.use(cors());
 
 // Initialize the main project folder
 
+app.use(express.static('website'));
+
 // Spin up the server
 // Callback to debug
+
+const port = 4000;
+
+const server = app.listen(port, () => {
+    console.log(`the server is running in port ${port}`)
+});
+
 
 // Initialize all route with a callback function
 
 // Callback function to complete GET '/all'
 
 // Post Route
+
+
   
--- website/app.js
+++ website/app.js
@@ -1,18 +1,47 @@
 /* Global Variables */
+let zipCode = document.getElementById('zip').value; 
+let feeling = document.getElementById('feelings').value;
+// Personal API Key for OpenWeatherMap API
+
+const APIkey = '526c32dd935943840aaf64738cac5c87';
+let baseURL = `https://api.openweathermap.org/data/2.5/weather?zip=${zipCode}&units=metric&appid=${APIkey}`;
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth()+1 +'.'+ d.getDate()+'.'+ d.getFullYear();
 
-// Personal API Key for OpenWeatherMap API
-
-// Event listener to add function to existing HTML DOM element
-
-/* Function called by event listener */
 
 /* Function to GET Web API Data*/
+
+const getData = async () => {
+    const response = await fetch(baseURL);
+    try{
+        const data = await response.json();
+        console.log(data.main.temp);
+    }
+    
+    catch(error){
+        console.log('error', error);
+    }
+}
 
 /* Function to POST data */
 
 
 /* Function to GET Project Data */
+
+
+/* Function to Update UI */
+
+
+// Event listener to add function to existing HTML DOM element
+/* Function called by event listener */
+
+document.getElementById('generate').addEventListener('click', () =>{
+    zipCode = document.getElementById('zip').value;
+    feeling = document.getElementById('feelings').value;
+    baseURL = `https://api.openweathermap.org/data/2.5/weather?zip=${zipCode}&units=metric&appid=${APIkey}`;
+    getData();
+});
+
+
