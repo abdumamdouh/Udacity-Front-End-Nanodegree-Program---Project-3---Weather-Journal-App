20880da17a55d3ca541449462dc6eb4c5783f95d
abdumamdouh <eabdogfx@gmail.com>
2021-05-13 05:38:10 +0200

    first files

---
+++ website/app.js
@@ -0,0 +1,18 @@
+/* Global Variables */
+
+// Create a new date instance dynamically with JS
+let d = new Date();
+let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+// Personal API Key for OpenWeatherMap API
+
+// Event listener to add function to existing HTML DOM element
+
+/* Function called by event listener */
+
+/* Function to GET Web API Data*/
+
+/* Function to POST data */
+
+
+/* Function to GET Project Data */
